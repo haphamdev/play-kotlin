e116771c2feee3456ac29fa30c892bd8ad919d01
Ha Pham <ha.pham.ext@personio.de>
2024-03-18 14:42:38 +0700

    Applying 'applicable addons' for v5/v6 plans

--- src/main/kotlin/ChargeBeeClient.kt
+++ src/main/kotlin/ChargeBeeClient.kt
@@ -1,137 +1,78 @@
 package org.example
 
-import com.chargebee.models.Item as CbItem
-import com.chargebee.models.ItemPrice as CbItemPrice
 import com.chargebee.Environment
 import com.chargebee.ListResult
 import com.chargebee.internal.ListRequest
-import com.chargebee.models.Coupon
 import com.chargebee.models.Item
-import com.chargebee.org.json.JSONArray
+import com.chargebee.models.Item as CbItem
 
-class ChargeBeeClient() {
+class ChargeBeeClient {
     companion object {
-        val BLACKLIST_COUPON_IDS = listOf(
-            "9%_discount_excl_epdv2",
-            "38%_discount_excl_epd_and_datev_and_rec",
-            "38%_discount_excl_epd_and_datev",
-            "35%_discount_excl_conversations",
-            "20%_discount_excl_datev",
-            "23%_discount_excl_conversations",
-            "30%_discount_excl_epd_v3",
-            "100%_discount_excl_epd_2",
-            "100%_discount_excl_epd",
-            "38%_discount_excl_epd",
-            "41%_discount_excl_conversations",
-            "42%_discount_excl_epd",
-            "10%_discount_excl_epd_new",
-            "15%_discount_excl_epd_v3",
-            "25%_discount_excl_epd_v3",
-            "57%_discount_excl_epd",
-            "43%_discount_excl_datev",
-            "9%_discount_excl_epd",
-            "18%_discount_excl_epd",
-            "32.5%_discount_excl_epd",
-            "35%_discount_excl_epd",
-            "30%_discount_excl_epd_v2",
-            "27%_discount_excl_epd",
-            "33%_discount_excl_epd",
-            "56%_discount_excl_epd",
-            "50%_discount_excl_epd",
-            "40%_discount_excl_epd",
-            "10%_discount_excl_epd",
-            "5%_discount_excl_epd",
-            "30%_discount_excl_epd",
-            "25%_discount_excl_epd_v2",
-            "20%_discount_excl_epd_v2",
-            "15%_discount_excl_epd_v2",
-            "8%_discount_excl_epd_v2",
-            "20%_discount_excl_epd_v1",
-            "8%_discount_excl_epd_temp",
-            "25%_discount_excl_epd_temp",
-            "15%_discount_excl_epd_temp",
-            "20%_discount_excl_epd_temp",
-            "35%DISCOUNTEXCLSURVEY",
-            "10%DISCOUNT(EXCL.EPD)V2",
-            "40%DISCOUNT(EXCL.EPD)V2",
-            "52.5%DISCOUNT(EXCL.EPD)SAETA",
-            "42.29%DISCOUNT(EXCL.EPD)",
+        val BLACKLIST_PLAN_TYPES = listOf(
+            "Key_Account_Management",
+            "job_posting_bundle",
+            "Success_Plans",
+            "Premium_Support"
+        )
+
+        val BLACKLIST_ADDON_TYPES = listOf(
+            "setup",
+            "Key_Account_Management",
+            "job_posting_bundle",
+            "personio_green",
+            "prof_services",
+            "volume_based_job_postings"
         )
     }
-    fun getAllItems(env: ChargebeeEnvironment): List<ChargebeeItem> = CbItem.list()
-        .status().`in`(Item.Status.ARCHIVED, Item.Status.ACTIVE)
-        .requestAllPages(env, "Fetching all items...")
-        .filter { !it.item().optString("cf_type").isNullOrEmpty() }
-        .map {
-            ChargebeeItem(
-                id = it.item().id(),
-                type = it.item().reqString("cf_type"),
-                isTrial = it.item().optString("cf_special_offer") == "trial",
-            )
-        }
-        .toList()
 
-    fun getAllItemPrices(env: ChargebeeEnvironment, itemIds: Set<String> = emptySet()): Sequence<ChargebeeItemPrice> =
-        CbItemPrice.list()
-            .status().`in`(CbItemPrice.Status.ARCHIVED, CbItemPrice.Status.ACTIVE)
-            .requestAllPages(env, "Fetching all Item prices...")
-            .filter {
-                itemIds.isEmpty() || itemIds.contains(it.itemPrice().itemId())
-            }
-            .map {
-                val ip = it.itemPrice()
-
-                ChargebeeItemPrice(
-                    id = ip.id(),
-                    itemId = ip.itemId(),
-                    isFree = ip.price() == 0L
-                        || (
-                        ip.price() == null
-                            && ip.tiers().all { tier -> tier.price() == null || tier.price() == 0L }
-                        ),
-                )
-            }
-
-    fun getAllLineItemDiscounts(env: ChargebeeEnvironment): Sequence<ChargebeeDiscount> = Coupon.list()
-        .status().`in`(Coupon.Status.ARCHIVED, Coupon.Status.ACTIVE)
-        .applyOn().`is`(Coupon.ApplyOn.EACH_SPECIFIED_ITEM)
-        .requestAllPages(env, "Fetching all Line-item discounts...")
+    fun getAllPlans(env: ChargebeeEnvironment): Sequence<ChargebeeItem> = CbItem.list()
+        .type().`is`(Item.Type.PLAN)
+        .status().`is`(Item.Status.ACTIVE)
+        .requestAllPages(env, "Fetching all plans...")
         .mapNotNull {
-            val coupon = it.coupon()
-
-            if (BLACKLIST_COUPON_IDS.contains(coupon.id())) {
-                return@mapNotNull null
-            }
-            val constraints = coupon.itemConstraints().mapNotNull { it.toDomainModel() }
-            val discountType = DiscountType.valueOf(coupon.discountType().name)
-            val discountValue = if (discountType == DiscountType.FIXED_AMOUNT) {
-                coupon.discountAmount().toDouble()
-            } else {
-                coupon.discountPercentage()
-            }
-
-            ChargebeeDiscount(
-                coupon.id(),
-                coupon.name(),
-                discountType,
-                discountValue,
-                ChargebeeDiscountApplyOn.EACH_SPECIFIED_ITEM,
-                constraints
+            val item = it.item()
+            val cfType = item.optString("cf_type")
+            val version = item.optInteger("cf_pricing_version")
+            if (cfType.isNullOrEmpty() ||
+                BLACKLIST_PLAN_TYPES.contains(cfType) ||
+                version == null ||
+                version < 5 || version > 6
+            ) return@mapNotNull null
+            ChargebeeItem(
+                id = item.id(),
+                type = cfType,
+                isTrial = it.item().optString("cf_special_offer") == "trial",
+                majorVersion = version,
             )
         }
 
-    private fun Coupon.ItemConstraint.toDomainModel(): ChargebeeItemConstraint? {
-        val targetType = ChargebeeItemConstraint.TargetType.valueOf(constraint().name)
-        val itemType = ChargebeeItemConstraint.ItemType.valueOf(itemType().name)
+    fun getAllAddons(env: ChargebeeEnvironment): Sequence<ChargebeeItem> = CbItem.list()
+        .type().`is`(Item.Type.ADDON)
+        .status().`is`(Item.Status.ACTIVE)
+        .requestAllPages(env, "Fetching all addons...")
+        .mapNotNull {
+            val item = it.item()
+            val cfType = item.optString("cf_type")
+            val version = item.optInteger("cf_pricing_version")
+            if (
+                cfType.isNullOrEmpty() ||
+                BLACKLIST_ADDON_TYPES.contains(cfType) ||
+                version == null ||
+                version < 5 || version > 6
+            ) return@mapNotNull null
+            ChargebeeItem(
+                id = item.id(),
+                type = cfType,
+                isTrial = it.item().optString("cf_special_offer") == "trial",
+                majorVersion = version,
+            )
+        }
 
-        val itemIds = if (targetType == ChargebeeItemConstraint.TargetType.SPECIFIC) {
-            itemPriceIds()?.map { it.toString() }?.toSet() ?: emptySet()
-        } else emptySet()
 
-        return ChargebeeItemConstraint(targetType, itemType, itemIds)
-    }
-
-    private fun ListRequest<*>.requestAllPages(chargebeeEnvironment: ChargebeeEnvironment, msg: String = "Receiving result from ChargeBee") =
+    private fun ListRequest<*>.requestAllPages(
+        chargebeeEnvironment: ChargebeeEnvironment,
+        msg: String = "Receiving result from ChargeBee"
+    ) =
         sequence<ListResult.Entry> {
             var nextOffset: String? = null
             do {
@@ -143,94 +84,24 @@ class ChargeBeeClient() {
             } while (nextOffset != null)
         }
 
-    fun updateDiscountConstraints(
+
+    fun updateApplicableAddonsForPlan(
         env: ChargebeeEnvironment,
-        discountId: String,
-        constraints: List<ChargebeeItemConstraint>,
+        planId: String,
+        addonIds: List<String>
     ) {
-        Coupon.updateForItems(discountId).apply {
-            constraints.forEachIndexed { idx, constraint ->
-                this.itemConstraintItemType(idx, Coupon.ItemConstraint.ItemType.valueOf(constraint.itemType.name))
-                this.itemConstraintConstraint(idx, Coupon.ItemConstraint.Constraint.valueOf(constraint.targetType.name))
-                if (constraint.targetType == ChargebeeItemConstraint.TargetType.SPECIFIC) {
-                    this.itemConstraintItemPriceIds(idx, JSONArray().putAll(constraint.itemIds))
-                }
-            }
-        }.request(env)
-
+        CbItem.update(planId)
+            .itemApplicability(Item.ItemApplicability.RESTRICTED)
+            .applicableItems(*addonIds.toTypedArray())
+            .request(env)
     }
 }
-
-data class ChargebeeDiscount(
-    val id: String,
-    val name: String,
-    val discountType: DiscountType,
-    val discountAmount: Double,
-    val applyOn: ChargebeeDiscountApplyOn,
-    val itemConstraints: List<ChargebeeItemConstraint> = emptyList(),
-) {
-    /**
-     * Check if the discount is applicable for an addon.
-     */
-    fun isApplicable(itemPrice: ChargebeeItemPrice): Boolean =
-        itemConstraints.firstOrNull { it.itemType == ChargebeeItemConstraint.ItemType.ADDON }
-            ?.isApplicable(itemPrice.id) ?: false
-
-    /**
-     * Check if the discount is applicable for a plan.
-     */
-}
-
-data class ChargebeeItemConstraint(
-    val targetType: TargetType,
-    val itemType: ItemType,
-    val itemIds: Set<String> = emptySet(),
-) {
-    /**
-     * The target type of constraint.
-     */
-    enum class TargetType(value: String) {
-        ALL("all"),
-        SPECIFIC("specific"),
-        NONE("none"),
-        CRITERIA("criteria"),
-    }
-
-    /**
-     * The item type that a constraint is targeting.
-     */
-    enum class ItemType(value: String) {
-        PLAN("plan"),
-        ADDON("addon"),
-        CHARGE("charge"),
-    }
-
-    /**
-     * Check if an item ID is allowed by the constraint.
-     */
-    fun isApplicable(itemId: String): Boolean = targetType == TargetType.ALL || itemIds.contains(itemId)
-}
-
-enum class DiscountType(val value: String) {
-    FIXED_AMOUNT("fixed_amount"),
-    PERCENTAGE("percentage"),
-}
-
-enum class ChargebeeDiscountApplyOn(val value: String) {
-    INVOICE_AMOUNT("invoice_amount"),
-    EACH_SPECIFIED_ITEM("each_specified_item"),
-}
-
-data class ChargebeeItemPrice(
-    val id: String,
-    val itemId: String,
-    val isFree: Boolean,
-)
 
 data class ChargebeeItem(
     val id: String,
     val type: String,
     val isTrial: Boolean,
+    val majorVersion: Int,
 )
 
 data class ChargebeeEnvironment(
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,218 +1,34 @@
 package org.example
 
-val ITEM_TYPE_GROUPS = listOf(
-    setOf("Core", "Core_Pro", "Core_Lite"),
-    setOf("Essential", "Professional", "Enterprise"),
-    setOf("conversations_standard", "conversations_standard", "conversations_premium_plus", "conversations_lite"),
-)
-
 const val DRY_RUN = true
 
 fun main() {
-<<<<<<< HEAD
-    val env = ChargebeeEnvironment("personio", "live_Ld9tqwNsoNlZSECTFxzUrSMdUHVZdwcdR")
-=======
-    val env = ChargebeeEnvironment("site-name", "api-key")
->>>>>>> refs/remotes/origin/prelease/1
-    val client = ChargeBeeClient()
+    val site  = System.getenv("CHARGEBEE_SITE")
+    val apiKey  = System.getenv("CHARGEBEE_API_KEY")
 
-    val items = client.getAllItems(env)
-    val itemsById = items.associateBy { it.id }
-    val itemPrices = client.getAllItemPrices(env, items.map { it.id }.toSet())
-    val itemPriceIdToItemType = itemPrices
-        .filter { itemsById.containsKey(it.itemId) }
-        .map { it.id to itemsById[it.itemId]!!.type }
-        .toMap()
+    val chargebeeEnv = ChargebeeEnvironment(site, apiKey)
+    val chargebeeClient = ChargeBeeClient()
+    val addonsGroupedByVersion = chargebeeClient.getAllAddons(chargebeeEnv)
+        .groupBy { it.majorVersion }
 
-    val trialItemPriceIds = itemPrices
-        .filter { it.isFree || (itemsById[it.itemId]?.isTrial ?: false) }
-        .map { it.id }
-        .toSet()
-
-    val itemPricesByTypes = itemPrices
-        .filter { itemsById.contains(it.itemId) && !trialItemPriceIds.contains(it.id) }
-        .groupBy { itemsById[it.itemId]!!.type }
-
-    client.getAllLineItemDiscounts(env)
-        .forEach { discount ->
-            var appliedToPlan = false
-            var appliedToAddon = false
-            var manualCheckNeeded = false
-
-            val updatedConstraints = discount.itemConstraints
-                .map { constraint ->
-                    when (constraint.itemType) {
-                        ChargebeeItemConstraint.ItemType.PLAN -> {
-                            if (constraint.targetType == ChargebeeItemConstraint.TargetType.NONE) return@map constraint
-                            appliedToPlan = true
-                            val checkResult = checkPlanConstraint(constraint, discount)
-                            if (checkResult.manualCheckNeeded) manualCheckNeeded = true
-                            return@map checkResult.constraint
-                        }
-
-                        ChargebeeItemConstraint.ItemType.ADDON -> {
-                            if (constraint.targetType == ChargebeeItemConstraint.TargetType.NONE) return@map constraint
-                            appliedToAddon = true
-                            val checkResult = checkAddonConstraint(
-                                constraint,
-                                discount,
-                                itemPriceIdToItemType,
-                                itemPricesByTypes,
-                                trialItemPriceIds
-                            )
-
-                            if (checkResult.manualCheckNeeded) manualCheckNeeded = true
-
-                            return@map checkResult.constraint
-                        }
-
-                        ChargebeeItemConstraint.ItemType.CHARGE -> {
-                            if (constraint.targetType != ChargebeeItemConstraint.TargetType.NONE) {
-                                println("Coupon ${discount.id} is applied to CHARGES. Manual check needed.")
-                                manualCheckNeeded = true
-                            }
-                            return@map constraint
-                        }
-                    }
-                }
-
-            if (appliedToPlan && appliedToAddon) {
-                manualCheckNeeded = true
-                println("Coupon ${discount.id} is applied to both PLANS and ADDONS. Manual check needed")
-            }
-
-            if (manualCheckNeeded) {
-                println("Skip coupon ${discount.id} for manual check")
-            } else if (updatedConstraints.any { !discount.itemConstraints.contains(it) }) {
-                if (DRY_RUN) {
-                    println("Skip updating coupon ${discount.id} in DRY RUN mode")
-                } else {
-                    println("Updating coupon ${discount.id}...")
-                    client.updateDiscountConstraints(env, discount.id, updatedConstraints)
-                    println("Updated coupon ${discount.id}")
-                }
-            } else {
-                println("Skip coupon ${discount.id}. No change needed")
-            }
-
-            println("----------------------------")
-        }
-}
-
-fun checkPlanConstraint(constraint: ChargebeeItemConstraint, discount: ChargebeeDiscount): ConstraintCheckResult {
-    when (constraint.targetType) {
-        ChargebeeItemConstraint.TargetType.ALL -> {
-            println("Coupon ${discount.id} is applied to all plans")
-            return ConstraintCheckResult(constraint)
-        }
-
-        ChargebeeItemConstraint.TargetType.SPECIFIC -> {
-            println("Coupon ${discount.id} is applied to ${constraint.itemIds.size} plans: ${constraint.itemIds}. Will be updated to All plans")
-            return ConstraintCheckResult(
-                ChargebeeItemConstraint(
-                    targetType = ChargebeeItemConstraint.TargetType.ALL,
-                    itemType = ChargebeeItemConstraint.ItemType.PLAN
-                )
-            )
-        }
-
-        ChargebeeItemConstraint.TargetType.CRITERIA -> {
-            println("Coupon ${discount.id} has criteria constraint for plans. Manual check needed")
-            return ConstraintCheckResult(constraint, true)
-        }
-
-        ChargebeeItemConstraint.TargetType.NONE -> return ConstraintCheckResult(constraint) //do nothing
+    addonsGroupedByVersion.keys.forEach { version ->
+        println(
+            "V$version addons:\n- " +
+                    addonsGroupedByVersion[version]!!.joinToString("\n- ") { it.id }
+        )
     }
-}
-
-/**
- * @return the constraint and whether manual check is needed
- */
-fun checkAddonConstraint(
-    constraint: ChargebeeItemConstraint,
-    discount: ChargebeeDiscount,
-    itemPriceIdToItemType: Map<String, String>,
-    itemPricesByTypes: Map<String, List<ChargebeeItemPrice>>,
-    trialItemPriceIds: Set<String>
-): ConstraintCheckResult {
-    when (constraint.targetType) {
-        ChargebeeItemConstraint.TargetType.ALL -> {
-            println("Coupon ${discount.id} is applied to ALL addon. Manual check is needed")
-            return ConstraintCheckResult(constraint, true)
-        }
-
-        ChargebeeItemConstraint.TargetType.SPECIFIC -> {
-            val itemPriceIds = constraint.itemIds
-
-            if (itemPriceIds.isEmpty()) {
-                println("Coupon ${discount.id} is applied to specific addons but no addon is selected. Manual check needed")
-                // change it to none instead when there is no item price ids, to avoid confusion
-                return ConstraintCheckResult(
-                    ChargebeeItemConstraint(
-                        targetType = ChargebeeItemConstraint.TargetType.NONE,
-                        itemType = constraint.itemType
-                    ),
-                    true
+    chargebeeClient.getAllPlans(chargebeeEnv)
+        .forEach { plan ->
+            val applicableAddonIds = addonsGroupedByVersion[plan.majorVersion]!!.map { it.id }
+            println("Updating plan '${plan.id}' with v${plan.majorVersion} addons")
+            if (!DRY_RUN) {
+                chargebeeClient.updateApplicableAddonsForPlan(
+                    env = chargebeeEnv,
+                    planId = plan.id,
+                    addonIds = applicableAddonIds
                 )
+                println("Updated plan '${plan.id}'")
             }
-
-            val applicableItemTypes = itemPriceIds.mapNotNull { itemPriceIdToItemType[it] }.distinct()
-
-            if (applicableItemTypes.size > 1) {
-                println("Coupon ${discount.id} is applied to ${applicableItemTypes.size} ${constraint.itemType} types: $applicableItemTypes")
-            }
-
-            val allItemPriceIdsByTypes = applicableItemTypes.flatMap { itemType ->
-                val itemTypeGroup =
-                    ITEM_TYPE_GROUPS.firstOrNull { it.contains(itemType) } ?: setOf(itemType)
-
-                itemTypeGroup.flatMap { subItemType ->
-                    itemPricesByTypes[subItemType]?.map {
-                        it.id
-                    } ?: emptyList()
-                }
-            }.toSet()
-
-            var finalItemPriceIds = itemPriceIds
-
-            // Find out the missing items
-            val missingItemPriceIds = allItemPriceIdsByTypes - itemPriceIds
-            if (missingItemPriceIds.isNotEmpty()) {
-                println("Coupon ${discount.id} - Missing item prices: $missingItemPriceIds")
-                finalItemPriceIds = finalItemPriceIds + allItemPriceIdsByTypes
-            }
-
-            if (finalItemPriceIds.any { trialItemPriceIds.contains(it) }) {
-                println(
-                    "Coupon ${discount.id}: Remove following free addons ${
-                        finalItemPriceIds.filter {
-                            trialItemPriceIds.contains(
-                                it
-                            )
-                        }
-                    }"
-                )
-                finalItemPriceIds = finalItemPriceIds.filter { !trialItemPriceIds.contains(it) }.toSet()
-            }
-
-            return if (finalItemPriceIds != itemPriceIds) {
-                ConstraintCheckResult(constraint.copy(itemIds = finalItemPriceIds))
-            } else {
-                ConstraintCheckResult(constraint)
-            }
-
         }
-
-        ChargebeeItemConstraint.TargetType.NONE -> return ConstraintCheckResult(constraint) // do nothing
-        ChargebeeItemConstraint.TargetType.CRITERIA -> {
-            println("Coupon ${discount.id} has criteria constraint for addons. Manual check needed")
-            return ConstraintCheckResult(constraint, true)
-        }
-    }
 }
-
-data class ConstraintCheckResult(
-    val constraint: ChargebeeItemConstraint,
-    val manualCheckNeeded: Boolean = false
-)
 
